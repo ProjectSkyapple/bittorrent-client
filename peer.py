bbcfca1622eea9cc0bc12f1e97c5a2c54c353e33
Aaron Jacob <36686926+ProjectSkyapple@users.noreply.github.com>
2025-12-03 23:46:51 -0600

    Start/stop peer server

--- peer.py
+++ peer.py
@@ -18,3 +18,35 @@ class PeerServer:
         
         self.running = False
         self.server_socket = None
+
+    def start(self):
+        """Start the server."""
+
+        self.running = True
+        thread = threading.Thread(target=self._serve)
+        thread.start()
+
+    def _serve(self):
+        """Main server loop to accept incoming connections from other peers."""
+
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            self.server_socket = s
+
+            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            s.bind((self.ip, self.port))
+            s.listen(50)  # TODO: Change queue size if needed
+
+            print(f"[{self.peer_id}] [Peer server] Listening on {self.ip}:{self.port}")
+
+            while self.running:
+                conn, addr = s.accept()
+                threading.Thread(target=self._handle_peer_connection, args=(conn, addr)).start()
+    
+    def stop(self):
+        """Stop the server."""
+
+        print(f"[{self.peer_id}] [Peer server] Stopping...")
+
+        self.running = False
+        if self.server_socket:
+            self.server_socket.close()
